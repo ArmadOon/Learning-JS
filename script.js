0c202faa8d5cefd03e6e1fc8734e2b71ecc9b8ad
Martin Pluhar <pluhar.martin@outlook.com>
2022-11-29 18:17:29 +0100

    scope chain

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-'Use strict';
-console.log('Hello World');
+'use strict';
+
+function calcAge(birthYear) {
+	const age = 2022 - birthYear;
+	function printAge() {
+		const output = `You are ${age}, born in ${birthYear}`;
+		console.log(output);
+	}
+	printAge();
+	return age;
+}
+
+const firstName = 'Martin';
+calcAge(1993);
