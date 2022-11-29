fb19eaee16c506e9b2877c873818fbff34590462
Martin Pluhar <pluhar.martin@outlook.com>
2022-11-29 20:06:29 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// // variables
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
+// var me = 'Martin';
+// let job = 'Programmer';
+// const year = 1993;
+
+// //functions
+// console.log(addDecl(2, 3));
+// console.log(addexpr(2, 3)); // Cannot access 'addexpr' before initialization
+// console.log(addArrow(2, 3)); // Cannot access 'addexpr' before initialization
+// function addDecl(a, b) {
+// 	return a + b;
+// }
+
+// const addexpr = function (a, b) {
+// 	return a + b;
+// };
+// const addArrow = (a, b) => a + b;
+// example;
+if (!numProduct) deleteShopingCart();
+var numProduct = 10;
+function deleteShopingCart() {
+	console.log('All products are deleted');
+}
