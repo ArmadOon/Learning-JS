88717824404667afa3624bcfc48dbbae3c2e92da
Martin Pluhar <pluhar.martin@outlook.com>
2022-12-04 14:02:08 +0100

    update

--- script.js
+++ script.js
@@ -25,6 +25,17 @@ const restaurant = {
 	}
 };
 
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+	name: restaurantName,
+	openingHours: hours,
+	categories: tags
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
 // const arr = [2, 3, 6];
 // //destructuring assigment
 // const [x, y, z] = arr;
