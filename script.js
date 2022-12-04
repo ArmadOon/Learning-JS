afe2d594f1ea708b694a988e669b4e111d002643
Martin Pluhar <pluhar.martin@outlook.com>
2022-12-04 12:38:26 +0100

    destructuring objects , starter

--- script.js
+++ script.js
@@ -8,48 +8,62 @@ const restaurant = {
 
 	order: function (starterIndex, mainIndex) {
 		return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+	},
+	openingHours: {
+		thu: {
+			open: 12,
+			close: 22
+		},
+		fri: {
+			open: 11,
+			close: 23
+		},
+		sat: {
+			open: 0, // Open 24 hours
+			close: 24
+		}
 	}
 };
 
-const arr = [2, 3, 6];
-//destructuring assigment
-const [x, y, z] = arr;
+// const arr = [2, 3, 6];
+// //destructuring assigment
+// const [x, y, z] = arr;
 
-console.log(x, y, z);
+// console.log(x, y, z);
 
-let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
 
-//*  switch main to secondary and secondary to main
-//! we need temporary variable
+// //*  switch main to secondary and secondary to main
+// //! we need temporary variable
 
-/* let temp = main;
-main = secondary;
-secondary = temp;
+// /* let temp = main;
+// main = secondary;
+// secondary = temp;
 
-console.log(main, secondary);
- */
+// console.log(main, secondary);
+//  */
 
-//! destructuring way
+// //! destructuring way
 
-[main, secondary] = [secondary, main];
-console.log(main, secondary);
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
 
-const [starterCourse, mainCourse] = restaurant.order(2, 0);
-console.log(starterCourse, mainCourse);
+// const [starterCourse, mainCourse] = restaurant.order(2, 0);
+// console.log(starterCourse, mainCourse);
 
-// nested array
+// // nested array
 
-const nested = [2, 3, [5, 6]];
-/* const [i, , j] = nested;
-console.log(i, j);
- */
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
+// const nested = [2, 3, [5, 6]];
+// /* const [i, , j] = nested;
+// console.log(i, j);
+//  */
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
 
-// Default values
-const [p, q, r] = [8, 9];
-console.log(p, q, r); // r is undefined
+// // Default values
+// const [p, q, r] = [8, 9];
+// console.log(p, q, r); // r is undefined
 
-const [o = 1, l = 2, m = 5] = [8, 9]; //default values
-console.log(o, l, m);
+// const [o = 1, l = 2, m = 5] = [8, 9]; //default values
+// console.log(o, l, m);
