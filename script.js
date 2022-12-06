a460145d12c38d12a87ce85f2acc17cf7105a40c
Martin Pluhar <pluhar.martin@outlook.com>
2022-12-06 19:56:40 +0100

    function

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 'Use strict';
-console.log('Hello World');
+
+// ! Object Oriented Programming
+
+//* Constructor function and new operator
+
+const Person = function (firstName, birthYear) {
+	this.firstName = firstName;
+	this.birthYear = birthYear;
+};
+const martin = new Person('Martin', 1993);
+console.log(martin);
+// ! STEPS :
+// * 1. New {} is created
+// * 2. function is called, this = {}
+// * 3. {} linked to prototype
+// * 4. function automatically return {}
